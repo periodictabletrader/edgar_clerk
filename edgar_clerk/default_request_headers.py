a8dc066d789ea3b56c74c411a64b48518313ad33
periodictabletrader <periodictabletrader@gmail.com>
2021-08-22 11:12:23 +0100

    Added default_request_headers.py with instructions on how to setup request_headers.py

---
+++ edgar_clerk/default_request_headers.py
@@ -0,0 +1,11 @@
+'''
+Populate the fields below and rename this file to request_headers.py.
+This is an identifier used on HTTP Requests made to the SEC EDGAR website. Any requests made to their website without
+these details will receive a standard message asking you to set this up.
+'''
+
+HEADERS = {
+    'User-Agent': '',   # You can find your user agent at https://whatmyuseragent.com/ and just copy/paste here
+                        # Or by searching "user agent" on your search engine and copying the string here
+    'From': ''          # This should be a valid email id
+}
